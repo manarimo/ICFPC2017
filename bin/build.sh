3b6c3ab276ab3aef48bc4eb2f08e7c7b4e70e8e0
Osamu Koga <osak.63@gmail.com>
2017-08-02 23:59:07 +0900

    Jenkinsfile test

---
+++ bin/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Building ${GIT_COMMIT} from ${GIT_BRANCH}"
+echo "Last build on this branch: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
---
+++ bin/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Running ${GIT_COMMIT} from ${GIT_BRANCH}"
