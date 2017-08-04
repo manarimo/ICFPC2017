63def740889c5c82a1cdb2a189d0b7d0d6b9d6e7
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-04 23:45:37 +0900

    removed space to fix araisan

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-GIT_COMMIT=$2
+set -e
+
 GIT_BRANCH=$1
+GIT_COMMIT=$2
 
 echo "Building ${GIT_COMMIT} from ${GIT_BRANCH}"
 
-./install || (bin/run_python.sh araisan "AIのビルドに失敗したのだ。。。(コミット: ${GIT_COMMIT}, ブランチ: ${GIT_BRANCH})" && exit 1)
+./install || (bin/run_python.sh araisan "AIのビルドに失敗したのだ。。。（コミット:${GIT_COMMIT},ブランチ:${GIT_BRANCH}）" && exit 1)
 
 echo "Done"
-bin/run_python.sh araisan "AIのビルドに成功したのだ！(コミット: ${GIT_COMMIT}, ブランチ: ${GIT_BRANCH})"
+bin/run_python.sh araisan "AIのビルドに成功したのだ！（コミット:${GIT_COMMIT},ブランチ:${GIT_BRANCH}）"
