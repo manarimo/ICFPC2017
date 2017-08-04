7665985d22dce83696dcfb3458d37f8e35c9d572
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-04 23:51:38 +0900

    save ai

--- bin/build.sh
+++ bin/build.sh
@@ -9,5 +9,12 @@ echo "Building ${GIT_COMMIT} from ${GIT_BRANCH}"
 
 ./install || (bin/run_python.sh araisan "AIのビルドに失敗したのだ。。。（コミット:${GIT_COMMIT},ブランチ:${GIT_BRANCH}）" && exit 1)
 
-echo "Done"
+echo "AI build done."
 bin/run_python.sh araisan "AIのビルドに成功したのだ！（コミット:${GIT_COMMIT},ブランチ:${GIT_BRANCH}）"
+
+AI_DEST=/var/ai/${GIT_COMMIT}/
+cp -r . ${AI_DEST} (bin/run_python.sh araisan "AIの保存に失敗したのだ。。。（コミット:${GIT_COMMIT},ブランチ:${GIT_BRANCH}）" && exit 1)
+
+ls ${AI_DEST}
+echo "AI saved at ${AI_DEST}"
+bin/run_python.sh araisan "AIの保存に成功したのだ！（コミット:${GIT_COMMIT},ブランチ:${GIT_BRANCH}）"
