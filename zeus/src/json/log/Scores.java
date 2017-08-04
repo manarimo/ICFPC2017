74cfd493547e70c74c3e0653e39e8de1c8e8157f
Mirai <mkut.at@gmail.com>
2017-08-05 06:32:23 +0900

    ろぐふぉーまっとへんこう

--- zeus/src/json/log/Scores.java
+++ zeus/src/json/log/Scores.java
@@ -1,13 +1,19 @@
 package json.log;
 
+import json.game.Map;
+import json.game.Move;
 import json.game.Score;
 
 import java.util.List;
 
 public class Scores {
+    public Map map;
+    public List<Move> moves;
     public List<Score> scores;
 
-    public Scores(final List<Score> scores) {
+    public Scores(final Map map, final List<Move> moves, final List<Score> scores) {
+        this.map = map;
+        this.moves = moves;
         this.scores = scores;
     }
 }
