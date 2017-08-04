278314aab3a21c84c94f55ba05f8c5398da17d4b
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-04 23:16:11 +0900

    added python requirements

--- bin/build.sh
+++ bin/build.sh
@@ -9,7 +9,7 @@ GIT_PREVIOUS_SUCCESSFUL_COMMIT=$3
 echo "Building ${GIT_COMMIT} from ${GIT_BRANCH}"
 echo "Last build on this branch: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
 
-./install || bin/run_python.sh araisan "AIのビルドに失敗したのだ。。。(コミット: ${GIT_COMMIT}, ブランチ: ${GIT_BRANCH})"
+./install || (bin/run_python.sh araisan "AIのビルドに失敗したのだ。。。(コミット: ${GIT_COMMIT}, ブランチ: ${GIT_BRANCH})" && exit 1)
 
 echo "Done"
 bin/run_python.sh araisan "AIのビルドに成功したのだ！(コミット: ${GIT_COMMIT}, ブランチ: ${GIT_BRANCH})"
