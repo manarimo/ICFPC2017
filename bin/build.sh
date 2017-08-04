fbc4bb16a76aca447348f106a101cf6b05f0689b
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-04 23:26:16 +0900

    removed unused vars in build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -4,10 +4,8 @@ set -e
 
 GIT_COMMIT=$1
 GIT_BRANCH=$2
-GIT_PREVIOUS_SUCCESSFUL_COMMIT=$3
 
 echo "Building ${GIT_COMMIT} from ${GIT_BRANCH}"
-echo "Last build on this branch: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
 
 ./install || (bin/run_python.sh araisan "AIのビルドに失敗したのだ。。。(コミット: ${GIT_COMMIT}, ブランチ: ${GIT_BRANCH})" && exit 1)
 
