3a48bc68f366616d021148f9152f44ec5653b748
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-05 16:01:18 +0900

    araisan do not fail without internet

--- src/python/araisan/__main__.py
+++ src/python/araisan/__main__.py
@@ -6,12 +6,16 @@ import sys
 SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
 SLACK_BOT_ICON_URL = "https://files.slack.com/files-pri/T6GV5GB71-F6J4Q2QEP/araisan.jpg"
 
+
 def post_slack(text):
-    requests.post(SLACK_WEBHOOK_URL, data=json.dumps({
-        'text': text,
-        'username': 'Arai-san',
-        'icon_url': SLACK_BOT_ICON_URL,
-    }))
+    try:
+        requests.post(SLACK_WEBHOOK_URL, data=json.dumps({
+            'text': text,
+            'username': 'Arai-san',
+            'icon_url': SLACK_BOT_ICON_URL,
+        }))
+    except:
+        print("failed to post to slack. no internet connection?")
 
 
 if __name__ == '__main__':
