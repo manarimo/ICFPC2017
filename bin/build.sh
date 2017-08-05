b5fcd98f9e8d562e856e94c4b72af423ac593d10
Osamu Koga <osak.63@gmail.com>
2017-08-06 02:28:25 +0900

    アライさん、発言権まで奪われてしまったのだ。でもがんばるのだ。

--- bin/build.sh
+++ bin/build.sh
@@ -5,13 +5,13 @@ set -ex
 GIT_BRANCH=$1
 GIT_COMMIT=$2
 COMMIT_MESSAGE=$(git show --format=%B -s)
+COMMITTER=$(git show --format=%cn -s)
 
 echo "Building ${GIT_COMMIT} from ${GIT_BRANCH}"
 
 ./install || (bin/run_python.sh araisan "AIのビルドに失敗したのだ。。。（${COMMIT_MESSAGE},ブランチ:${GIT_BRANCH},コミット:${GIT_COMMIT}）" && exit 1)
 
 echo "AI build done."
-bin/run_python.sh araisan "AIのビルドに成功したのだ！（${COMMIT_MESSAGE},ブランチ:${GIT_BRANCH},コミット:${GIT_COMMIT}）"
 
 AI_DEST=/var/ai/${GIT_COMMIT}/
 mkdir -p ${AI_DEST}
@@ -20,4 +20,4 @@ rsync -a --exclude='.*' . ${AI_DEST} || (bin/run_python.sh araisan "AIの保存�
 
 ls ${AI_DEST}
 echo "AI saved at ${AI_DEST}"
-bin/run_python.sh araisan "AIの保存に成功したのだ！（${COMMIT_MESSAGE},ブランチ:${GIT_BRANCH},コミット:${GIT_COMMIT}）"
+bin/run_python.sh araisan "${COMMITTER}が「${COMMIT_MESSAGE}」と言っているのだ！"
