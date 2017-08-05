3ebdb640c2f21ec6176bde169aaddb38463956b6
Mirai <mkut.at@gmail.com>
2017-08-05 18:20:18 +0900

    athena

--- zeus/src/json/game/Site.java
+++ zeus/src/json/game/Site.java
@@ -7,6 +7,12 @@ public class Site {
     public double x;
     public double y;
 
+    public Site(final int id, final double x, final double y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
     @JsonIgnore
     public int getId() {
         return id;
