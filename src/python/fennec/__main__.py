dc3c9e767f4bd8107b176f9dca0251256d68cc0a
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-05 13:47:05 +0900

    fennec only tagged feature

--- src/python/fennec/__main__.py
+++ src/python/fennec/__main__.py
@@ -92,7 +92,7 @@ def main():
     if args.ais is not None:
         ai_commits += args.ais.split(',')
     tags = tag_names()
-    to_sample = tags.values() if args.only_tagged else list_ais()
+    to_sample = list(tags.values()) if args.only_tagged else list_ais()
     ai_commits += random.sample(to_sample, args.random_ai_num)
     ai_commands = [ai_command(tags.get(commit, commit)) for commit in ai_commits]
     for i in range(args.repeat):
