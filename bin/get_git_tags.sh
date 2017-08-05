a08dd08a63b230f5017b319b8de64b5a7087eb36
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-06 06:45:59 +0900

    reverted tags

--- bin/get_git_tags.sh
+++ bin/get_git_tags.sh
@@ -4,8 +4,6 @@ SCRIPT=$(readlink -f $0)
 SCRIPT_DIR=`dirname $SCRIPT`
 ROOT_DIR=`dirname $SCRIPT_DIR`
 cd $ROOT_DIR
-git tag -l | xargs git tag -d > /dev/null
-git fetch origin --tags 2> /dev/null
 for tag in $(git tag) ; do
     echo ${tag}
     echo $(git show-ref -s ${tag})
