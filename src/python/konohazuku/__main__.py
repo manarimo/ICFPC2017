cf025a687f312354c4608d6a4788a0d5fc9e46d9
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-06 17:59:50 +0900

    konoha speaks mysql

--- src/python/konohazuku/__main__.py
+++ src/python/konohazuku/__main__.py
@@ -4,6 +4,9 @@ from collections import Counter, defaultdict
 import elo
 import pandas as pd
 import numpy as np
+import MySQLdb
+from MySQLdb.cursors import DictCursor
+from argparse import ArgumentParser
 
 
 LOGS_DIR = Path("/var/local/logs/")
@@ -15,7 +18,28 @@ def prob(win, all, draw):
     return win / nall if nall > 0 else 0.
 
 
-def fetch_results():
+def fetch_results_db():
+    with MySQLdb.connect(host="35.194.126.173", user="root", passwd="kaban", db="adlersprung") as conn:
+        cursor = conn.cursor(DictCursor)
+        cursor.execute("SELECT * FROM match_log")
+        for row in cursor.fetchall():
+            log_data = json.loads(row["log"])
+            if "tag_names" not in log_data:
+                continue
+            all_names = log_data["tag_names"]
+            if "scores" not in log_data:
+                continue
+            raw_scores = [-1] * len(all_names)
+            for sc in log_data["scores"]:
+                raw_scores[sc["punter"]] = sc["score"]
+            rank_scores = [len([s for s in raw_scores if s <= score]) for score in raw_scores]
+            punter_rank_scores = defaultdict(list)
+            for name, rank_score in zip(all_names, rank_scores):
+                punter_rank_scores[name].append(rank_scores)
+            yield punter_rank_scores
+
+
+def fetch_results_file():
     for meta_path in LOGS_DIR.iterdir():
         if not meta_path.name.endswith("meta.json"):
             continue
@@ -29,6 +53,11 @@ def fetch_results():
             continue
         for sc in meta_json["scores"]:
             punter_rank_scores[all_names[sc["punter"]]].append(sc["rank_score"])
+        yield punter_rank_scores
+
+
+def fetch_results(punter_rank_score_collection):
+    for punter_rank_scores in punter_rank_score_collection:
         if len(punter_rank_scores) != 2:
             continue
         names = list(punter_rank_scores.keys())
@@ -41,13 +70,23 @@ def fetch_results():
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument("--use-db", action="store_true", help="Use Japari Library (MySQL DB)")
+    args = parser.parse_args()
+
     print("meta analysis tool konohazuku")
+    if args.use_db:
+        print("using database")
+        data_source = fetch_results_db()
+    else:
+        print("using file system data")
+        data_source = fetch_results_file()
     kati = Counter()
     draws = Counter()
     per_win_agg = Counter()
     per_draw_agg = Counter()
     per_match_agg = Counter()
-    for names, scores in fetch_results():
+    for names, scores in fetch_results(data_source):
         if scores[0] > scores[1]:
             kati[(names[0], names[1])] += 1
             per_win_agg[names[0]] += 1
