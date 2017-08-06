26272dbcc12486ddbe7fb7894987f2a76e09d6e0
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-06 14:34:56 +0900

    最新のサンドスターだよ、まだ周りがキラキラしてるでしょ？

--- bin/run_ai.sh
+++ bin/run_ai.sh
@@ -6,6 +6,13 @@ GIT_COMMIT=$1
 shift
 AI_DIRECTORY=/var/ai/${GIT_COMMIT}/
 
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=`dirname $SCRIPT`
+ROOT_DIR=`dirname $SCRIPT_DIR`
+
+# replace sandstar with latest version
+cp $ROOT_DIR/bin/sandstar.rb $AI_DIRECTORY/bin/sandstar
+
 cd ${AI_DIRECTORY}
 ./punter $@
 
