f6f941864db5ddab7eb96d9d0a8711f996e7cbc7
y3eadgbe <yosukey@indeed.com>
2017-08-06 14:29:41 +0900

    とりあえず繰り返し1回

--- src/python/tsuchinoko/__main__.py
+++ src/python/tsuchinoko/__main__.py
@@ -12,14 +12,12 @@ LOG_DIR = Path(ROOT_DIR / "logs")
 
 def exe(map_path: Path, ai_commands, ruleset=None):
     ruleset = ruleset or []
-    print("ruleset:", ruleset)
     ruleset_args = ["-{}".format(rule) for rule in ruleset]
     cmd = ["java", "-cp", "/var/icfpc/zeus/build:/var/icfpc/zeus/lib/*", "Main"]
     cmd += ruleset_args
     cmd.append(str(map_path.absolute()))
     cmd.append(str(len(ai_commands)))
     cmd += ai_commands
-    print(cmd)
     out = subprocess.check_output(cmd, stderr=subprocess.DEVNULL)
     out_obj = json.loads(out.decode("utf-8"))
     scores = {}
@@ -45,7 +43,7 @@ def ruleset(ruleset_str):
         return []
     return ruleset_str.strip().split(',')
 
-REPEAT = 10
+REPEAT = 1
 NUM_PLAYERS = [2, 4, 8]
 MAPS = ["lambda", "randomMedium", "randomSparse", "rand1", "rand3"]
 RANDOM_AI = "6758e6b36e9b185501ea5d2731b98a5f396f2c67"
@@ -64,19 +62,21 @@ def main():
 
     ai_commit = tags.get(args.ai, args.ai)
     log = {"ai": args.ai, "ai_commit": ai_commit, "version": BENCHMARK_VERSION, "performances": []}
+    print("Benchmark Version: {}".format(BENCHMARK_VERSION))
+    print("AI: {}, Ruleset: {}".format(args.ai, args.ruleset))
     for punters in NUM_PLAYERS:
         ai_commits = [ai_commit] + [RANDOM_AI] * (punters - 1)
         ai_commands = [ai_command(commit) for commit in ai_commits]
         for map in MAPS:
-            print("punters: {}, map: {}".format(punters, map))
+            print("punters: {}, map: {}".format(punters, map), flush=True)
             total_score = 0
             scores = []
             for i in range(REPEAT):
                 score = exe(Path("map/{}.json".format(map)), ai_commands, args.ruleset)
                 scores.append(score)
                 total_score += score
-                print("Score: {}".format(score))
-            print("Total Score: {}".format(total_score))
+                print("Score: {}".format(score), flush=True)
+            print("Total Score: {}".format(total_score), flush=True)
             testcase_name = "{}-{}".format(map, punters)
             log["performances"].append({"name": testcase_name, "map": map, "punters": punters, "total": total_score, "scores": scores})
     for performance in log["performances"]:
