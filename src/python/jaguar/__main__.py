d3cdff0b740ea6968f1f9638507efd4842cf2392
Shunsuke Ohashi <shunsuke@indeed.com>
2017-08-07 19:16:52 +0900

    jaguar ai

--- src/python/jaguar/__main__.py
+++ src/python/jaguar/__main__.py
@@ -1,10 +1,139 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.externals import joblib
 from washimimizuku.eval_model import feature_vector, edge_key
+from pathlib import Path
+import sys
+import base64
+import pickle
+from io import StringIO
 
 
-def main():
-    print("jaguar: machine learning based punter.")
+# TODO: embed this model within the script because the punter to read a file is prohibited.
+CLASSIFIER_PATH = Path(Path(__file__).parent / "clf.pkl")
+
+
+class Game(object):
+    def __init__(self, graph, mines, edge_owner, punters, punter_id, edges):
+        self.graph = graph
+        self.mines = mines
+        self.edge_owner = edge_owner
+        self.punters = punters
+        self.punter_id = punter_id
+        self.edges = edges
+
+    def feature_vector(self):
+        return feature_vector(self.graph, self.mines, self.edge_owner, self.punter_id, 3, 10000)
+
+
+class State(object):
+    def __init__(self, game):
+        self.x = 1
+
+    def dumps(self):
+        return base64.b64encode(pickle.dumps(self)).decode()
+
+
+def read_game(f):
+    def read_int():
+        return int(f.readline())
+    punters = read_int()
+    punter_id = read_int()
+    nodes = read_int()
+    mine_count = read_int()
+    mines = []
+    for i in range(mine_count):
+        mines.append(read_int())
+    edge_count = read_int()
+    graph = {i: [] for i in range(nodes)}
+    edge_owner = {}
+    edges = []
+    for i in range(edge_count):
+        items = [int(c) for c in f.readline().split()]
+        if len(items) == 4:
+            fr, to, ow1, ow2 = items
+        else:
+            fr, to, ow1 = items
+        graph[fr].append(to)
+        graph[to].append(fr)
+        if ow1 == -1:
+            ow1 = None
+        edge_owner[edge_key(fr, to)] = ow1
+        edges.append(edge_key(fr, to))
+    return Game(graph, mines, edge_owner, punters, punter_id, edges)
+
+
+def read_state(f):
+    encoded = f.readline().strip().encode()
+    return pickle.loads(base64.b64decode(encoded))
+
+
+def handshake(f):
+    print("jaguar")
+
+
+def init(f):
+    game = read_game(f)
+    state = State(game)
+    print(0)  # futures
+    print(state.dumps())
+
+
+def move(f):
+    game = read_game(f)
+    state = read_state(f)
+    clf = joblib.load(CLASSIFIER_PATH)
+    hands = [(-1e100, -1)]
+    for i, edge in enumerate(game.edges):
+        if game.edge_owner[edge] is not None:
+            continue
+        game.edge_owner[edge] = game.punter_id
+        fv = game.feature_vector()
+        hands.append((clf.predict([fv])[0], i))
+        game.edge_owner[edge] = None
+    print(max(hands)[1])
+    print(state.dumps())
+
+
+def end(f):
+    pass
+
+
+def main(f=sys.stdin):
+    mode = f.readline().strip()
+    if mode == "HANDSHAKE":
+        handshake(f)
+    elif mode == "INIT":
+        init(f)
+    elif mode == "MOVE":
+        move(f)
+    elif mode == "END":
+        end(f)
+
+
+TEST_INIT = """\
+INIT
+2
+0
+3
+1
+0
+2
+0 1 0
+1 2 -1
+"""
+
+TEST_MOVE = """\
+MOVE
+2
+0
+3
+1
+0
+2
+0 1 -1
+1 2 -1
+gANjX19tYWluX18KU3RhdGUKcQApgXEBfXECWAEAAAB4cQNLAXNiLg==
+"""
 
 
 if __name__ == '__main__':
-    main()
+    main(StringIO(TEST_MOVE))
